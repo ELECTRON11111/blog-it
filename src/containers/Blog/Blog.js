8bf79e854a363c99b0f48a3b8fc217d605e09f60
daniel_eln <omoniyiopemipo6@gmail.com>
2022-10-10 01:04:35 +0100

    sending a delete request with axios and local error handling with the catch method for promises

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        postSelectedId: null
+        postSelectedId: null,
+        error: false
     }
 
     componentDidMount() {
@@ -26,6 +27,10 @@ class Blog extends Component {
                 });
                 // We can call setState here and won't cause unnecessary re-renders
                 this.setState({posts: updatedPosts});
+            })
+            .catch(error => {
+                // console.log(error);
+                this.setState({error: true});
             });
         
         // DON'T -  call setState here because it causes re-renders 
@@ -36,14 +41,19 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post 
-                key={post.id} 
-                title={post.title} 
-                author={post.author} 
-                clicked={() => this.postSelectedHandler(post.id)}
-            />
-        })
+        let posts = <p style={{textAlign: "center"}}>Something went wrong</p>
+
+        if(!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post 
+                    key={post.id} 
+                    title={post.title} 
+                    author={post.author} 
+                    clicked={() => this.postSelectedHandler(post.id)}
+                />
+            });
+        }
+        
         return (
             <div>
                 <section className="Posts">
